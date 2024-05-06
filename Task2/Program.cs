ae02fb7b9d537612aa03ef10383d0f6f73437472
Emin <emin@example.com>
2024-05-07 00:54:05 +0400

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,166 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region   Task1
+            //Console.WriteLine("Birinci ededi daxil edin: ");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Ikinci ededi daxil edin");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x} + {y} = {x+y}");
+            //Console.WriteLine($"{x} - {y} = {x - y}");
+            //Console.WriteLine($"{x} * {y} = {x * y}");
+
+            #endregion
+
+            #region  Task2
+            Console.WriteLine("Saniyeni daxil edin: ");
+            int gunSaniye = 86400;
+            int verilenSaniye = int.Parse(Console.ReadLine());
+
+            int gunuTap = verilenSaniye / gunSaniye;
+            int deqiqeTap = verilenSaniye % gunSaniye;
+            int saat = deqiqeTap / 3600;
+            int verilenSaniye2 = deqiqeTap % 3600;
+            int deqiqe = verilenSaniye2 / 60;
+            int saniye = verilenSaniye2 % 60;
+
+            Console.WriteLine(gunuTap + " Gun");
+            Console.WriteLine(saat + " Saat");
+            Console.WriteLine(deqiqe + " Deqiqe");
+            Console.WriteLine(saniye + " Saniye");
+
+
+
+
+
+
+
+            #endregion
+
+            #region Task3
+            //Console.WriteLine("Yashi daxil edin:");
+            //int age = int.Parse(Console.ReadLine());
+            //if (age >= 1 && age <= 6)
+            //{
+            //    Console.WriteLine("Ushaq yash qrupu");
+            //}
+            //else if (age > 6 && age <= 17)
+            //{
+            //    Console.WriteLine("Shagird yash qrupu");
+            //}
+            //else if (age >= 18 && age <=35)
+            //{
+            //    Console.WriteLine("Genc yash qrupu");
+            //}
+            //else if (age > 35 && age <= 63)
+            //{
+            //    Console.WriteLine("Yetkinlik yash qrupu");
+            //}
+            //else if (age > 63)
+            //{
+            //    Console.WriteLine("Teqaudchu");
+            //}
+            #endregion
+
+            #region Task4
+            //Console.WriteLine("Gun daxil edin:");
+            //char day = char.Parse(Console.ReadLine());
+            //switch (day)
+            //{
+            //    case '1':
+            //        Console.WriteLine("Bazar ertesi");
+            //        break;
+            //    case '2':
+            //        Console.WriteLine("Chershenbe axshami");
+            //        break;
+            //    case '3':
+            //        Console.WriteLine("Chershenbe");
+            //        break;
+            //    case '4':
+            //        Console.WriteLine("Cume axshami");
+            //        break;
+            //    case '5':
+            //        Console.WriteLine("Cume");
+            //        break;
+            //    case '6':
+            //        Console.WriteLine("Shenbe");
+            //        break;
+            //    case '7':
+            //        Console.WriteLine("Bazar");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Sehv eded daxil etdiniz!");
+            //        break;
+            //}
+            #endregion
+
+            #region Task5
+            //Console.WriteLine("Eded daxil edin:");
+            //int month = int.Parse(Console.ReadLine());
+
+            //switch(month)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Yanvar, 31 gun");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Fevral, 28 gun");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Mart, 31 gun");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Aprel, 30 gun");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("May, 31 gun");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Iyun, 30 gun");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Iyul, 31 gun");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("Avqust, 31 gun");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("Sentyabr, 30 gun");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("Oktyabr, 31 gun");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("Noyabr, 30 gun");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("Dekabr, 31 gun");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Sehv eded daxil etdiniz!");
+            //        break;
+
+            //}
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
